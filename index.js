925ec69d4e9b3c98034fa25690d719df5ad2f449
xps13 <tlswlsdntks@naver.com>
2025-05-11 14:12:09 +0900

    주석과 세미콜론

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ console.log(1);
 console.log(true);
 console.log({name: '홍길동', age: 20, married: false});
 console.log('Hi!', 100, true, [1,2,3]);
+
+console.log('Hello');
+// 사뿐히 즈려밟고 가시옵소서
+// console.log('나를 밟고 가라');
+console.log('World');
+
+// 한 줄 짜리 주석
+console.log('Hello'); // 이렇게 코드 옆에 다는 것도 가능
+
+/*
+  주석은 이와 같이 여러 줄에 걸쳐 작성할 수도 있습니다.
+  console.log('안찍힘');
+  console.log('이것도 안찍힘');
+  장문의 주석을 달거나 위와 같이 코드의 일부를 주석처리하여 비활성화하는 등의 목적으로 사용됩니다.
+*/
+console.log('찍힘');
+console.log('찍힘');
